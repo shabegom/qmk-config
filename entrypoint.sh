93b35814b30ee1ad6e27933c2a72ef121cce7c96
Phoscur <phoscur@pheelgood.net>
2022-01-12 23:26:27 +0100

    Add cp $INPUT_PATH -> keymaps

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,10 @@ echo "Setting up ZSA QMK fork ..."
 qmk setup zsa/qmk_firmware -b firmware20
 echo "Adding keymap ..."
 qmk new-keymap -kb moonlander -km neo
+echo "Overwrite keymap layout"
+ls $INPUT_PATH
+cp $INPUT_PATH/* /qmk_firmware/keyboards/moonlander/keymaps/neo
 echo "Compiling ..."
 qmk compile -kb moonlander -km neo
 echo "Done!"
-# outfile: .build/moonlander_neo.hex
+# outfile: .build/moonlander_neo.bin
