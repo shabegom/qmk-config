4779f881ea3741911003397e4e6c1a67c37d46b2
Phoscur <phoscur@pheelgood.net>
2022-01-17 17:35:21 +0100

    Fix qmk setup

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
-FROM qmkfm/qmk_firmware
-
+FROM qmkfm/qmk_cli
+ADD layout_src ./layout_src
+RUN qmk setup zsa/qmk_firmware -b firmware20 -y
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,15 @@
-#!/bin/bash -l
+#!/bin/bash
 
-echo "Setting up ZSA QMK fork ..."
-qmk setup zsa/qmk_firmware -b firmware20
+#echo "Setting up ZSA QMK fork ..."
+# moved into Dockerfile, TODO make configurable
+#qmk setup zsa/qmk_firmware -b firmware20
 echo "Adding keymap ..."
 qmk new-keymap -kb moonlander -km neo
-qmk compile -kb moonlander -km neo
 echo "Overwrite keymap layout"
-ls $INPUT_PATH
-cp $INPUT_PATH/* /qmk_firmware/keyboards/moonlander/keymaps/neo
+LAYOUT_PATH="${INPUT_PATH:-layout_src}"
+ls $LAYOUT_PATH
+echo "Path $LAYOUT_PATH"
+cp $LAYOUT_PATH/* /qmk_firmware/keyboards/moonlander/keymaps/neo
 ls /qmk_firmware/keyboards/moonlander/keymaps
 echo "Compiling neo  ..."
 qmk compile -kb moonlander -km neo
