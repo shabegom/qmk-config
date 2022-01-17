f58bce8be96292385a1f542192f8b3b4ee5174b6
Phoscur <phoscur@pheelgood.net>
2022-01-17 18:59:34 +0100

    Fix cp built files into artifacts

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,15 @@
 #qmk setup zsa/qmk_firmware -b firmware20
 echo "Adding keymap ..."
 qmk new-keymap -kb moonlander -km neo
-echo "Overwrite keymap layout"
 LAYOUT_PATH="${INPUT_PATH:-layout_src}"
-ls $LAYOUT_PATH
-echo "Path $LAYOUT_PATH"
+ARTIFACTS_PATH="${INPUT_ARTIFACTS_PATH:-artifacts}"
+echo "Overwrite keymap layout from $LAYOUT_PATH"
 cp $LAYOUT_PATH/* /qmk_firmware/keyboards/moonlander/keymaps/neo
 ls /qmk_firmware/keyboards/moonlander/keymaps
 echo "Compiling neo  ..."
 qmk compile -kb moonlander -km neo
 # outfile: .build/moonlander_neo.bin
-echo "Copying out into artifacts ..."
-cp -r .build/* $INPUT_ARTIFACTS_PATH/*
+echo "Copying out into artifacts folder ..."
+ls /qmk_firmware/.build
+cp -r /qmk_firmware/.build/* $ARTIFACTS_PATH/*
 echo "Done!"
